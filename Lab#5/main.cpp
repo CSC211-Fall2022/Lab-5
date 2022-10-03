439af7958b14531b4d059470cf8da6c0d8f82ac1
Maxim Voyevoda <maximvoyevoda@gmail.com>
2022-10-02 20:02:40 -0400

    should be fucking working.............. BUT NOT

--- Lab#5/main.cpp
+++ Lab#5/main.cpp
@@ -1,59 +1,64 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-#include <vector>
 #include <stdlib.h>
 #include <fstream>
 using namespace std;
 
 struct menuItemType {
-   
    string menuItem;
    double menuPrice;
    int count;
-    
 };
 
 menuItemType menuList[8];
-
 ifstream fin;
 
+int numberOfOrders;
+int itemFromMenu;
+int numberOfItems;
 
-void getData(){
+void getData();
+void showMenu();
+void printCheck();
+
+int main() {
    
-   string scan;
-   string name;
-   string price;
+   cin >> numberOfOrders;
+    
+   fin.open("menu.txt");
    
-   int c = 0;
+   getData();
+   showMenu();
+   printCheck();
    
-   while (getline(fin, scan)){
-//       cout << scan << endl;
-      name = "";
-      price = "";
-      bool name_end = false;
-      for (long unsigned int i = 0; i < scan.size(); i++){
-         //  cout << "scan[i]: " << scan[i] << endl;
-         //checks if menu item name has ended
-          if (scan[i + 1] == '$'){
-              name_end = true;
-          } else if (name_end == false){
-              name+= scan[i];
-          } else if (scan[i] != '$'){
-              price+= scan[i];
-          }
-          
-      }
-          
-      menuList[c].menuItem = name;
-      menuList[c].menuPrice = stod(price);
-       
-//       cout << menuList[c].menuItem << endl;
-//       cout << menuList[c].menuPrice << endl;
-      c++;
+   fin.close();
    
+   return 0;
+}
+
+void printCheck(){
+ 
+   double total = 0.00;
+ 
+   cout << endl << "Your check:\n";
+   
+   for (int i = 0; i < numberOfOrders; i++){
+      
+      cin >> itemFromMenu;
+      cin >> numberOfItems;
+      cout << left << setw(16) << menuList[itemFromMenu - 1].menuItem << '\t' << numberOfItems << " $" << menuList[itemFromMenu - 1].menuPrice * numberOfItems << endl;
+      total += (menuList[itemFromMenu - 1].menuPrice * numberOfItems);
+      
    }
-     
+   
+   double tax = total * 0.086;
+   double aDue = total + tax;
+   
+   cout << left << setw(19) << "Amount Total" << '$' << total << endl;
+   cout << left << setw(19) << "Tax" << '$' << tax << endl;
+   cout << left << setw(19) << "Amount Due" << '$' << aDue;
+   
 }
 
 void showMenu(){
@@ -70,55 +75,35 @@ void showMenu(){
    
 }
 
-int numberOfOrders;
-int itemFromMenu;
-int numberOfItems;
+void getData(){
+   
+   string scan;
+   string name;
+   string price;
+   
+   int c = 0;
+   
+   while (getline(fin, scan)){
 
-void printCheck(){
- 
+      name = "";
+      price = "";
+      bool name_end = false;
+      for (long unsigned int i = 0; i < scan.size(); i++){
+         //checks if menu item name has ended
+          if (scan[i + 1] == '$'){
+              name_end = true;
+          } else if (name_end == false){
+              name+= scan[i];
+          } else if (scan[i] != '$'){
+              price+= scan[i];
+          }
+          
+      }
+          
+      menuList[c].menuItem = name;
+      menuList[c].menuPrice = stod(price);
+      c++;
    
-   double total = 0.00;
- 
-   cout << endl << "Your check:\n";
-   
-   for (int i = 0; i < numberOfOrders; i++){
-      
-      cin >> itemFromMenu;
-      cin >> numberOfItems;
-      cout << left << setw(16) << menuList[itemFromMenu - 1].menuItem << '\t' << numberOfItems << " $" << menuList[itemFromMenu - 1].menuPrice * numberOfItems << endl;
-      total += (menuList[itemFromMenu - 1].menuPrice * numberOfItems);
-      
    }
-   
-   // cout << fixed << setprecision(10);
-   double tax = total * 0.087;
-   double aDue = total + tax;
-   // cout << fixed << setprecision(2);
-   
-   cout << left << setw(19) << "Amount Total" << '$' << total << endl;
-   cout << left << setw(19) << "Tax" << '$' << tax << endl;
-   cout << left << setw(19) << "Amount Due" << '$' << aDue;
-   
-}
-
-int main() {
-   
-   cin >> numberOfOrders;
-    
-    fin.open("menu.txt");
-   
-   getData();
-   
-   
-   showMenu();
-   
-   printCheck();
-   
-   
-   
-   
-   
-   
-   
-   return 0;
+     
 }
